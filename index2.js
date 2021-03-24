ed9b40030f13c03983d954d17c93c6fd73d6a5dd
xilevolauu <3qauto1234@gmail.com>
2021-03-25 01:21:44 +0700

    mo ta

--- index2.js
+++ index2.js
@@ -4,7 +4,7 @@ let pass = document.getElementById("pass");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (user.value == "admin" && pass.value == 123){
-        window.location.href="https://xilevolauu.github.io/travis-blogs.github.io/login.html"
+        window.location.href="https://xilevolauu.github.io/travis-blogs.github.io/index2.html"
     }
     else{
         alert("Tên đăng nhập hoặc mật khẩu sai")
